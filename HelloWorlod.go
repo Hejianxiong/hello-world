29b087a9e992489ca735c73ae8baa2db79cea304
HeJianxiong <jianxiong2017@gmail.com>
2020-02-08 09:22:23 +0800

    add

---
+++ HelloWorlod.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+    //"fmt"
+    "net/http"
+)
+
+func main() {
+    http.HandleFunc("/hello", func(w http.ResponseWriter, r *http.Request) {
+        w.Write([]byte("hello world!"))
+    })
+
+    http.ListenAndServe("127.0.0.1:8080", nil)
+}
